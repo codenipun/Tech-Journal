fd90a8daff06e2ea4f7d30c215b2e8bbbdeb1a04
Nipun Jain <nipunjain119@gmail.com>
2022-12-23 02:15:10 +0530

    final commit1

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', async (req, res)=>{
 });
 
 app.use('/articles', articleRouter);
- 
-app.listen(5000, ()=>{
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
     console.log("server started at port 5000");
 });
